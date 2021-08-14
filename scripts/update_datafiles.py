f63348e44fc06d55583f8f1b583b585ce52ca55c
sansbacon <eric@erictruett.com>
2021-08-14 13:13:16 -0500

    updated script

--- scripts/update_datafiles.py
+++ scripts/update_datafiles.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python 
 from pathlib import Path
+import pandas as pd
+
 
 def run():
-    pth = Path(__file__).parent.parent
-    for f in pth.glob('*'):
+    basepth = Path(__file__).parent.parent
+    pth = basepth / 'nflfastR-data' / 'data'
+    df = pd.concat([pd.read_parquet(f) for f in pth.glob('play_by_play*.parquet')])
+    df.to_parquet('playbyplay', partition_cols=['season'])
+    for f in (basepth / 'playbyplay').glob('*'):
         print(f)
 
 
