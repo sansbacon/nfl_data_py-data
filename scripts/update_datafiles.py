857c2127e894d7b1eae368c7c59e1608feab4fcd
sansbacon <eric@erictruett.com>
2021-08-15 20:59:04 -0500

    Broke out script into serial read of parquet files

--- scripts/update_datafiles.py
+++ scripts/update_datafiles.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python 
 from pathlib import Path
-import pyarrow as pa
-import pyarrow.parquet as pq
-import pyarrow.dataset as ds
-
+import numpy as np
+import pandas as pd
 
 def run():
     basepth = Path(__file__).parent.parent
     (basepth / "pbp").mkdir(exist_ok=True)
     pth = basepth / 'nflfastR-data' / 'data'   
     for f in pth.glob('play_by_play*.parquet'):
-        dataset = ds.parquet_dataset(str(f))
-        part = ds.partitioning(
-          pa.schema([("season", pa.int32())]),
-          flavor="hive"
-        )
-        
-        ds.write_dataset(dataset, basepth / 'pbp', format="parquet", partitioning=part)
+        df = pd.read_parquet(f)
+        cols = df.select_dtypes('float')
+        df.loc[:, cols] = df.loc[:, cols].astype(np.float32)
+        df.to_parquet(basepth / 'pbp', partition_cols=['season'])
 
 
 if __name__ == '__main__':
