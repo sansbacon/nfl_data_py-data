7882babf18925ca13a6e9953a7466d3c5a646ba5
sansbacon <eric@erictruett.com>
2021-08-15 20:08:37 -0500

    added partition write code

--- scripts/update_datafiles.py
+++ scripts/update_datafiles.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python 
 from pathlib import Path
-import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.dataset as ds
 
 
 def run():
     basepth = Path(__file__).parent.parent
-    pth = basepth / 'nflfastR-data' / 'data'
-    df = pd.concat([pd.read_parquet(f) for f in pth.glob('play_by_play*.parquet')])
-    df.to_parquet('playbyplay', partition_cols=['season'])
-    for f in (basepth / 'playbyplay').glob('*'):
-        print(f)
+    (basepth / "pbp").mkdir(exist_ok=True)
+    pth = basepth / 'nflfastR-data' / 'data'   
+    for f in pth.glob('play_by_play*.parquet'):
+        dataset = ds.parquet_dataset(str(f))
+        part = ds.partitioning(
+          pa.schema([("season", pa.int32())]),
+          flavor="hive"
+        )
+        
+        ds.write_dataset(dataset, basepth / 'pbp', format="parquet", partitioning=part)
 
 
 if __name__ == '__main__':
